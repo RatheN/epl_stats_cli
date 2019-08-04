fe8c7789b2928cfc77c02220ab3f879b3c34eb6c
Nathan Rathe <nprathe10@gmail.com>
2019-08-04 07:20:16 +0000

    added team.rb and edited scraper

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,3 +1,10 @@
 class Scraper
+  def self.scrape_index_page
+    doc = Nokogiri::HTML(open("https://sports.ndtv.com/english-premier-league/epl-table/2018-19"))
+    teams_list = []
 
+    doc.css('.nd-table nd-accord-head active').each do |t|
+      name = t.css('.nd-table-td tbl-team').span.text
+    end
+  end
 end
--- lib/team.rb
+++ lib/team.rb
@@ -1,3 +1,17 @@
 class Team
+  attr_accessor :name, :rank, :goals_for, :goals_against, :points
+  @@all = []
 
+  def initialize(name = nil, rank = nil, goals_for = nil, goals_against = nil, points = nil)
+    @name = name
+    @rank = rank
+    @goals_for = goals_for
+    @goals_against = goals_against
+    @points = points
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 end
